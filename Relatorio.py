bf3bce4562da209f18fc1b7f52d792a87c7b498a
Jamily Emanuelle <jamilyemanuelle181@gmail.com>
2024-12-08 20:44:50 -0400

    Modificações

--- Relatorio.py
+++ Relatorio.py
@@ -1,12 +1,25 @@
-# Colocar a importaân da classe e a relaçaõ
-from datetime import date, time
+from collections import defaultdict
+from datetime import datetime
+
+class GerenciadorRelatorio:
+  def __init__(self):
+    self.__relatorio=defaultdict(list)
+
+
 class Relatorio:
   def __init__(self, id, tipo, dataEntrada, dataSaida=None, visitantes=0):
-    self.__id= id
-    self.__tipo= tipo
-    self.__dataEntrada = dataEntrada
-    self.__dataSaida = dataSaida
-    self.__Visitantes = visitantes
+    try:
+      if not isinstance(dataEntrada, datetime):
+        raise TypeError(" Data é um objeto datetime")
+      self.__id= id
+      self.__tipo= tipo
+      self.__dataEntrada = dataEntrada
+      self.__dataSaida = None
+      self.__Visitantes = visitantes
+    except TypeError as e:
+      print(f" erro de criação do relatório: {e}")
+    finally:
+      print("final da inicialização do relatório")
     
   def get_idRelatorio(self):
     return self.__id
@@ -23,19 +36,43 @@ class Relatorio:
   def get_quantVisitantes(self):
     return self.__Visitantes
 
-# Função para exibir o Relatório
 def exibirRelatorio(self):
-  print(f"ID:{self.__id}")
-  print(f"Tipo:{self.__tipo}")
-  print(f"Tipo:{self.__dataEntrada.strftime('%Y-%m-%d %H:%M:%S')}")
-if self.__dataSaida:  # Se a saída foi registrada
-   print(f"Saída: {self.__dataSaida.strftime('%Y-%m-%d %H:%M:%S')}")
-else:
-  print("Saída: Não registrada.")
-  print(f"Visitantes: {self.__visitantes}")
+  try:
+    print(f"ID:{self.__id}")
+    print(f"Tipo:{self.__tipo}")
+    print(f"Tipo:{self.__dataEntrada.strftime('%Y-%m-%d %H:%M:%S')}")
+    if self.__dataSaida:
+      print(f"Saída: {self.__dataSaida.strftime('%Y-%m-%d %H:%M:%S')}")
+    else:
+      print("Saída: Não registrada.")
+      print(f"Visitantes: {self.__visitantes}")
+  except Exception as e:
+    print(f"Erro na exibição do relatório")
+  finally:
+    print("Exibição finalizada")
 
- # Função para registrar a saída
-def registrar_saida(self, saida):
-  self.__saida = saida
-  
+
+def registrar_saida(self, dataSaida):
+  try:
+    if not isinstance(dataSaida, datetime):
+      raise TypeError("A data de saída é um obejeto datetime")
+    self.__dataSaida=dataSaida
+  except TypeError as e:
+    print(f"Erro ao registrar a saída:{e}")
+  finally:
+    print("Processo de registro de saída concluido")
+
+def lista_relatorio(self, modo=None):
+  try:
+    if modo and modo not in self.__relatorio:
+      raise ValueError(f"Nhenum relatório voltado para esse modo {'modo'}")
+    relatorio= self.__relatorio[modo] if modo else self.__relatorio
+    for modo, lista in relatorio.items():
+      print(f"\n Modo: {modo}")
+    for rel in lista:
+      rel.exibirRelatorio()
+  except ValueError as e:
+    print(f"Erro ao exibir relatório lista:{e}")
+  finally:
+    print("Listagem realizada")
     
--- main.py
+++ main.py
@@ -2,15 +2,23 @@
 #Turma: 2º ano informática vespertino
  
 import time
-from catraca import *
-
+from Aluno import *
+from Catraca import *
+from Aluno import *
+from Recepcionista import *
+from Relatorio import GerenciadorRelatorio, Relatorio
+from UsuarioIFRO import *
+from Visitante import *
+from datetime import datetime, timedelta
 time.sleep(0.1)
 print("*=*" * 16)
 print("Olá,Seja bem-vindo(a) ao nosso Sistema IDCotrol.")
 print("*=*"*16)
 time.sleep(0.5)
 while True:
-    adicionar=input("\nVocê vai ser cadastrar na área aluno ou servidor?\n\n[A]Aluno\n[B]Servidor\n[C]Visitante\n\nResposta=")
+    acesso=int(input("Você deseja acessar cadastro ou relatório?\n [1]Cadastro\n[2]relatório"))
+    if acesso()==1:
+      adicionar=input("\nVocê vai ser cadastrar na área aluno ou servidor?\n\n[A]Aluno\n[B]Servidor\n[C]Visitante\n\nResposta=")
     if adicionar.upper() == "A":
       print("*=*"*12)
       print("Você vai se cadastrar na área aluno")
@@ -24,6 +32,7 @@ while True:
       print("Agora você tem acesso a todos os serviços da catraca")
       print("."*15)
       break
+
     elif adicionar.upper()=="B":
       print("*=*"*12)
       print("Você vai se cadastra na área do servidor")
@@ -40,7 +49,6 @@ while True:
       visitante= Visitante(nome="", cpf="",idade="", documento="")
       visitante.cadastrar_visitante()
       visitante.exibirDados_Visitante()
-    
       break
       
 # Caso a pessoa escolher a opção errada
@@ -48,3 +56,31 @@ while True:
     if adicionar.upper() not in ["A", "B", "C"]:
       print("Resposta inválida, tente novamente")
       continue
+    elif acesso==2:
+      print("\n=== ÁREA DE RELATÓRIO ===")
+      gerencia= GerenciadorRelatorio()
+      rela1= Relatorio(1,"Aluno", datetime.now(), alunos=15)
+      rela2=Relatorio(2,"Visitante", datetime.now - timedelta(hours=2))
+      rela3= Relatorio(3,"Servidor", datetime.now() - timedelta(days=1), servidor=5)
+      rela4=Relatorio(4,"Visitante", datetime.now() -  timedelta(days=2), visitantes=1)
+
+      gerencia.adicionar_relatorio(rela1)
+      gerencia.adicionar_relatorio(rela2)
+      gerencia.adicionar_relatorio(rela3)
+      gerencia.adicionar_relatorio(rela4)
+
+      print("\n=== RELATÓRIOS ====")
+      gerencia.lista_relatorio()
+
+      print("\n=== SAÍDA ===")
+      rela2.saida(datetime.now())
+      rela2.exibirRelatorio()
+
+      print("\n=== RELATÓRIO MODO 'VISITANTE'===")
+      gerencia.lista_relatorio("Visitante")
+
+      print("\n=== RELATÓRIO MODO 'SERVIDOR'===")
+      gerencia.lista_relatorio("Servidor")
+
+      print("\n=== RELATÓRIO MODO 'ALUNO'===")
+      gerencia.lista_relatorio("Aluno")
